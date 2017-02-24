a665f9d999c4675d01372f519e3f78a733f5e2ef
Cameron Corda <ccorda@gmail.com>
2017-02-24 14:33:00 -0800

    better error handling, stop deploy and return user to branch
    closes #31

--- _build/deploy.sh
+++ _build/deploy.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-## GIT Remote Variables
+## Per Project Variables -- CUSTOMIZE THESE FIRST
 STAGING_REMOTE="git@git.wpengine.com:staging/bubs.git"
 PRODUCTION_REMOTE="git@git.wpengine.com:production/bubs.git"
+GIT_EMAIL="hello+bubs@patronage.org"
+GIT_NAME="Bubs Deploy"
 
-## Branch for continuous dev deployment (also update .travis.yml)
-DEV_BRANCH="staging"
-
-## SH function to read txt files with list of files to include/exclude
-getArray() {
+# read txt files with list of files to include/exclude
+function get_array {
     i=0
     while read line # Read a line
     do
@@ -17,18 +16,27 @@ getArray() {
     done < $1
 }
 
-## build script
+# handle script errors, exit and kick you to working branch
+function error_exit {
+    echo "$1" 1>&2
+    echo "Aborting and returning to working branch."
+    git stash
+    git checkout $branch
+    exit 1
+}
+
+# check environment to ensure we should proceed with build
 if [[ -n $(git status --porcelain) ]]; then
-    echo "There are uncommited changes -- please commit before proceeding with testing"
+    error_exit "There are uncommited changes -- please commit before proceeding."
 elif [ ! -z "$TRAVIS_BRANCH" ] && [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-    echo "This is a pull request, don't build"
+    error_exit "This is a pull request, don't build"
 else
 
     ## on travis, add credentials
     if [ ! -z "$TRAVIS_BRANCH" ]; then
         echo -e "Host git.wpengine.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-        git config --global user.email "accounts@patronage.org"
-        git config --global user.name "Patronage"
+        git config --global user.email ${GIT_EMAIL}
+        git config --global user.name ${GIT_NAME}
     fi
 
     if [ `git branch --list deploy` ]; then
@@ -36,11 +44,11 @@ else
        git branch -D deploy
     fi
 
-    #save current branch to a variable
+    # save current branch to a variable
     branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 
     git checkout -b deploy
-    gulp release
+    gulp release || error_exit "Gulp release failed."
 
     echo "Adding built files that are normally .gitignored..."
     array=()
@@ -77,7 +85,7 @@ else
         echo "Pushing to staging..."
         git remote add staging ${STAGING_REMOTE}
         git push -f staging deploy:master
-        echo "Returning to working branch..."
+        echo "Returning to working branch."
         git stash
         git checkout $branch
 
@@ -85,22 +93,20 @@ else
         echo "Pushing to production..."
         git remote add production ${PRODUCTION_REMOTE}
         git push -f production deploy:master
-        echo "Returning to working branch..."
+        echo "Returning to working branch."
         git stash
         git checkout $branch
 
     else
-        echo "No deploy condition met"
+        error_exit "No deploy conditions met."
     fi
 
     ## test for git response, and if error code, bail out of travis with an error code
     success=$?
     if [[ $success -eq 0 ]];
     then
-        echo "Great success!"
+        echo "Build script complete"
     else
-        echo "Something went wrong with the deploy"
-        exit 1
-        echo "should have exited by now"
+        error_exit "Something went wrong with the deploy."
     fi
 fi
