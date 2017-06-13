e7fe562529f523b6d9f08e90bf90d9b964197a4d
Yuval Maman <yuviliom@gmail.com>
2017-06-13 14:36:22 -0400

    instead of js use markup for mobile/desktop

--- wp-content/themes/timber/assets/js/main.js
+++ wp-content/themes/timber/assets/js/main.js
@@ -19,36 +19,11 @@ var quickShareInit = function(){
     quickShare();
 };
 
-var headerNavPosition = function(inMobileMq){
-    // in mobile, the middle images are under the second large image . in desktop they are under the first one
-    if ( inMobileMq.matches ){ // mobile
-        $('.main-header-nav-desktop-holder .main-header-nav').appendTo( $('.main-header-nav-mobile-holder') );
-    } else { //desktop
-        $('.main-header-nav-mobile-holder .main-header-nav').appendTo($('.main-header-nav-desktop-holder'));
-    }
-};
-
-var headerInit = function(){
-    mobileMq = window.matchMedia('(max-width: 767px)'); //are we in mobile?
-
-    //initial
-    headerNavPosition(mobileMq);
-
-    //on resize
-    mobileMq.addListener(function(mql){
-        headerNavPosition(mql);
-    });
-
-};
-
-// init without document.ready
-
 // init within document.ready
 (function($) {
 
     fitVidInit();
     matchHeightInit();
-    headerInit();
     smoothScrollInit();
     quickShareInit();
     analyticsSourcing();
