cf121170d5fa8dd487d814e281abb3b0cb1b8b06
Cameron Corda <ccorda@gmail.com>
2017-09-28 14:53:35 -0700

    hide acf if not in dev
    closes #80

--- wp-content/themes/timber/setup/helpers/admin.php
+++ wp-content/themes/timber/setup/helpers/admin.php
@@ -29,6 +29,9 @@ function set_post_order_in_admin( $wp_query ) {
 }
 add_filter('pre_get_posts', 'set_post_order_in_admin' );
 
+//
+// Yoast
+//
 
 // Don't show WP SEO stuff in post list
 add_filter( 'wpseo_use_page_analysis', '__return_false' );
@@ -40,6 +43,14 @@ function lower_wpseo_priority() {
 }
 add_filter( 'wpseo_metabox_prio', 'lower_wpseo_priority' );
 
+//
+// ACF
+//
+
+// custom image sizes (used to provide thumnail sizes for ACF)
+add_image_size( 'Three By One', 375, 125, true );
+add_image_size( 'Three By Two', 180, 120, true );
+add_image_size( 'Hero Image', 400, 179, true );
 
 // Allows upload to Media Library with these file types
 function custom_mime_types($mimes) {
@@ -48,10 +59,22 @@ function custom_mime_types($mimes) {
 }
 add_filter('upload_mimes', 'custom_mime_types');
 
-// Hide comments from backend
-function admin_menus() {
-    remove_menu_page( 'edit-comments.php' );
+
+// Hide ACF in production to ensure version controlled JSON is truth
+// http://awesomeacf.com/snippets/hide-the-acf-admin-menu-item-on-selected-sites/
+function awesome_acf_hide_acf_admin() {
+
+    // check if the current env is dev
+    if ( defined('WP_ENV') && WP_ENV == "development" ) {
+        // show the acf menu item
+        print ("show");
+        return true;
+    } else {
+        // hide the acf menu item
+        print ("hide");
+        return false;
+    }
 }
-add_action('admin_menu', 'admin_menus');
+add_filter('acf/settings/show_admin', 'awesome_acf_hide_acf_admin');
 
 ?>
