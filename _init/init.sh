c2df089ae0ead1f4be6166b4a49f8b8835677db9
Cameron Corda <ccorda@gmail.com>
2017-09-28 22:55:36 -0700

    Move post install to a shell script.
    refs #40

---
+++ _init/init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+rm -rf ./composer/wp-content/themes/twenty*
+rm -rf ./composer/wp-config-sample.php
+rm -rf ./composer/wp-content/plugins/hello.php
+cp -R ./composer/* ./
+
+cp -n _init/wp-config-sample.php wp-config.php 2>/dev/null || :
+cp -n _init/wp-config-local-sample.php wp-config-local.php 2>/dev/null || :
+cp -n _init/gulpconfig.json gulpconfig.json 2>/dev/null || :
+cp -n _init/local.sql _data/local.sql 2>/dev/null || :
+cp -n _init/local-plugins.php wp-content/mu-plugins/local-plugins.php 2>/dev/null || :
+mv _init/README.md README.md 2>/dev/null || :
+
+mkdir -p wp-content/uploads && chmod 777 wp-content/uploads
+mkdir -p wp-content/themes/timber/acf-json && chmod -R 777 wp-content/themes/timber/acf-json
+chmod +x _build/deploy.sh
