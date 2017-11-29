104005b4481d4fb0e24473fc00b6164a3f108fba
Yuval Maman <yuviliom@gmail.com>
2017-11-29 12:15:41 -0500

    match bs dropdown markup better. fix typo on item.get_children. fix spacing on dropdown. add hoverintent js for better dropdown hovering

--- wp-content/themes/timber/assets/js/main.js
+++ wp-content/themes/timber/assets/js/main.js
@@ -15,9 +15,21 @@ var matchHeightInit = function(){
     $('.js-match-height').matchHeight();
 };
 
+var headerNav = function(){
+    // better dropdown hover intent ( inspired by https://stackoverflow.com/a/42183824/462002 )
+    $('.main-header').on('mouseenter mouseleave','.dropdown',function(e){
+      var $dropdown =$( e.target ).closest('.dropdown');
+      $dropdown.addClass('show');
+      setTimeout(function(){
+        $dropdown[ $dropdown.is(':hover') ? 'addClass' : 'removeClass' ]('show');
+      } , 300);
+    });
+};
+
 // init within document.ready
 (function($) {
 
+    headerNav();
     fitVidInit();
     matchHeightInit();
     smoothScrollInit();
