dd0eda09f4ab5331c66d5c6670c8ecc694a0f73d
Yuval Maman <yuviliom@gmail.com>
2020-09-30 12:38:23 -0400

    composer setup + plugin upgrades derived from recent Quarter 3 upgrades

--- _init/init.sh
+++ _init/init.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-rm -rf ./composer/wp-content/themes/twenty*
-rm -rf ./composer/wp-config-sample.php
-rm -rf ./composer/wp-content/plugins/hello.php
+# clear plugins to force new versions (excluding non-Wordpress plugin composer-libs/ )
+shopt -s extglob
+rm -rf ./wp-content/plugins/!(composer-libs)
+
+# Clean composer before copying . roots/wordpress installs the wordpress core into wp/ . Update wordpress
+rm -rf ./composer/wp/wp-config-sample.php
+rm -rf ./composer/wp/wp-content/
+mv -n ./composer/wp/* ./composer
+rm -rf ./composer/wp/
+
+
 cp -R ./composer/* ./
 
 cp -n _init/wp-config-sample.php wp-config.php 2>/dev/null || :
