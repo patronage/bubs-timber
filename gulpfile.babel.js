ba1c8d2a404a8977280f64d793ed512cf4c74086
Chris Herold <cmherold@gmail.com>
2020-11-11 14:43:02 -0800

    allow for no staticheaders, update packages

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,20 +1,20 @@
-'use strict';
+"use strict";
 
 // load gulp and gulp plugins
-import gulp from 'gulp';
-import plugins from 'gulp-load-plugins';
+import gulp from "gulp";
+import plugins from "gulp-load-plugins";
 const $ = plugins();
 
-$.sass.compiler = require('node-sass');
+$.sass.compiler = require("node-sass");
 
 // load node modules
-import del from 'del';
-import pump from 'pump';
-import beeper from 'beeper';
-import { argv } from 'yargs';
+import del from "del";
+import pump from "pump";
+import beeper from "beeper";
+import { argv } from "yargs";
 
 // others
-import browserSync from 'browser-sync';
+import browserSync from "browser-sync";
 const server = browserSync.create();
 //
 // Gulp config
@@ -23,7 +23,7 @@ const server = browserSync.create();
 // Override defaults with custom local config
 
 try {
-    var localConfig = require('./gulpconfig.json');
+    var localConfig = require("./gulpconfig.json");
 } catch (err) {
     var localConfig = {
         bs: {
@@ -31,38 +31,37 @@ try {
             logLevel: "info",
             tunnel: "",
             open: true,
-            notify: false
-        }
+            notify: false,
+        },
     };
 }
 
 // Build themeDir variable based on current theme from config file, fallback to default
 
-const themeRoot = 'wp-content/themes/';
-const theme = localConfig.activeTheme || 'timber';
+const themeRoot = "wp-content/themes/";
+const theme = localConfig.activeTheme || "timber";
 const themeDir = themeRoot + theme;
 
 // Defaults
 
 const config = {
     theme: themeDir,
-    assets: themeDir + '/assets',
-    dist: themeDir + '/dist',
-    dev: themeDir + '/dev',
-    output: themeDir + '/dev', // default to dev
-    static: themeDir + '/static',
-    init: './_init'
+    assets: themeDir + "/assets",
+    dist: themeDir + "/dist",
+    dev: themeDir + "/dev",
+    output: themeDir + "/dev", // default to dev
+    static: themeDir + "/static",
+    init: "./_init",
 };
-
 
 // in production tasks, we set this to true
 let isProduction = false;
 
 // Error Handling
 
-const handleErrors = err => {
-
-    if (err.message){ // note the error on console as well
+const handleErrors = (err) => {
+    if (err.message) {
+        // note the error on console as well
         console.log(err.message);
     }
 
@@ -75,14 +74,14 @@ const handleErrors = err => {
 
     // notifications
     if (err.line && err.column) {
-        var notifyMessage = 'LINE ' + err.line + ':' + err.column + ' -- ';
+        var notifyMessage = "LINE " + err.line + ":" + err.column + " -- ";
     } else {
-        var notifyMessage = '';
+        var notifyMessage = "";
     }
 
     $.notify({
-        title: 'FAIL: ' + err.plugin,
-        message: notifyMessage + err.message
+        title: "FAIL: " + err.plugin,
+        message: notifyMessage + err.message,
     });
 
     beeper(); // System beep
@@ -97,29 +96,34 @@ const handleErrors = err => {
 // Gulp Tasks
 //
 
-const styles = done => {
+const styles = (done) => {
     const sassOptions = {
-        outputStyle: 'expanded'
+        outputStyle: "expanded",
     };
 
-    pump([gulp.src(config.assets + '/scss/*.scss', { sourcemaps: !isProduction }),
-    $.sass(sassOptions),
-    $.if(isProduction, $.autoprefixer('last 2 versions')),
-    $.if(isProduction, $.csso()),
-    gulp.dest(config.output + '/css', { sourcemaps: !isProduction }),
-    server.stream()
-    ], err => {
-        if (err) {
-            handleErrors(err);
-            return done();
-        } else {
-            return done();
+    pump(
+        [
+            gulp.src(config.assets + "/scss/*.scss", {
+                sourcemaps: !isProduction,
+            }),
+            $.sass(sassOptions),
+            $.if(isProduction, $.autoprefixer("last 2 versions")),
+            $.if(isProduction, $.csso()),
+            gulp.dest(config.output + "/css", { sourcemaps: !isProduction }),
+            server.stream(),
+        ],
+        (err) => {
+            if (err) {
+                handleErrors(err);
+                return done();
+            } else {
+                return done();
+            }
         }
-    });
-}
+    );
+};
 
-const scripts = done => {
-
+const scripts = (done) => {
     const assets = $.useref({
         searchPath: config.assets,
         noconcat: true,
@@ -127,123 +131,154 @@ const scripts = done => {
 
     const uglifyOptions = {
         mangle: false,
-        compress: false
+        compress: false,
     };
 
-    const renameOptions = path => {
+    const renameOptions = (path) => {
         path.dirname = "js";
     };
 
-    const f = $.filter(['**', '!**/assets/vendor/**', '!**/assets/js/lib/**'], { 'restore': true });
-
-    pump([gulp.src(config.theme + '/views/layout.twig'),
-        assets,
-    $.debug({ 'title': 'debug: ', 'showFiles': argv.debug }), //to debug files getting proccessed
-    $.filter(['**', '!**/layout.twig'], { 'restore': true }),
-        f,
-    $.jshint(),
-    $.jshint.reporter('jshint-stylish'),
-    $.jshint.reporter('fail'),
-    f.restore,
-    $.rename(renameOptions),
-    $.babel({
-        presets: ['@babel/preset-env']
-    }),
-    $.uglify(uglifyOptions),
-    gulp.dest(config.output)
-    ], err => {
-        if (err) {
-            handleErrors(err);
-            return done();
-        } else {
-            return done();
-        }
+    const f = $.filter(["**", "!**/assets/vendor/**", "!**/assets/js/lib/**"], {
+        restore: true,
     });
-}
+
+    pump(
+        [
+            gulp.src(config.theme + "/views/layout.twig"),
+            assets,
+            $.debug({ title: "debug: ", showFiles: argv.debug }), //to debug files getting proccessed
+            $.filter(["**", "!**/layout.twig"], { restore: true }),
+            f,
+            $.jshint(),
+            $.jshint.reporter("jshint-stylish"),
+            $.jshint.reporter("fail"),
+            f.restore,
+            $.rename(renameOptions),
+            $.babel({
+                presets: ["@babel/preset-env"],
+            }),
+            $.uglify(uglifyOptions),
+            gulp.dest(config.output),
+        ],
+        (err) => {
+            if (err) {
+                handleErrors(err);
+                return done();
+            } else {
+                return done();
+            }
+        }
+    );
+};
 
 // When scripts runs, it creates extra files in copying from vendor we can delete
-const cleanScripts = done => {
-    del.sync([config.output + '/wp-content']);
+const cleanScripts = (done) => {
+    del.sync([config.output + "/wp-content"]);
     done();
-}
+};
 
 // copy unmodified files
-const copy = done => {
-    pump([gulp.src(config.assets + '/{img,fonts,js}/**/*', { base: config.assets }),
-    $.newer(config.output),
-    gulp.dest(config.output)
-    ], done);
-}
+const copy = (done) => {
+    pump(
+        [
+            gulp.src(config.assets + "/{img,fonts,js}/**/*", {
+                base: config.assets,
+            }),
+            $.newer(config.output),
+            gulp.dest(config.output),
+        ],
+        done
+    );
+};
 
 // loops through the generated html and replaces all references to static versions
-const rev = done => {
-    pump([gulp.src(config.dist + '/{css,js,fonts,img}/**/*'),
-    $.revAll.revision({ dontSearchFile: ['.js'] }),
-    gulp.dest(config.static),
-    $.revAll.manifestFile(),
-    gulp.dest(config.static)],
-        err => {
+const rev = (done) => {
+    pump(
+        [
+            gulp.src(config.dist + "/{css,js,fonts,img}/**/*"),
+            $.revAll.revision({ dontSearchFile: [".js"] }),
+            gulp.dest(config.static),
+            $.revAll.manifestFile(),
+            gulp.dest(config.static),
+        ],
+        (err) => {
             if (err) {
                 handleErrors(err);
                 return done();
             } else {
                 return done();
             }
-        });
-}
+        }
+    );
+};
 
 // write far futures expires headers for revved files
-const staticHeaders = done => {
-    pump([gulp.src(config.init + '/static.htaccess', { base: '' }),
-    $.rename(".htaccess"),
-    gulp.dest(config.static)],
-        err => {
+const staticHeaders = (done) => {
+    pump(
+        [
+            gulp.src(config.init + "/static.htaccess", {
+                base: "",
+                allowEmpty: true,
+            }),
+            $.rename(".htaccess"),
+            gulp.dest(config.static),
+        ],
+        (err) => {
             if (err) {
                 handleErrors(err);
                 return done();
             } else {
                 return done();
             }
-        });
-}
+        }
+    );
+};
 
 // clean output directory
-export const clean = done => {
+export const clean = (done) => {
     del.sync([config.dev, config.static, config.dist]);
     done();
-}
+};
 
-const serve = done => {
+const serve = (done) => {
     server.init({
         proxy: localConfig.bs.proxy,
         notify: localConfig.bs.notify || false,
         open: localConfig.bs.open || true,
         tunnel: localConfig.bs.tunnel || false,
-        logLevel: localConfig.bs.logLevel || 'info'
+        logLevel: localConfig.bs.logLevel || "info",
     });
 
-    gulp.watch(config.theme + '/**/*.{twig,php}', reload);
-    gulp.watch(config.assets + '/scss/**/*.scss', styles);
-    gulp.watch(config.assets + '/js/**/*.js', gulp.series(scripts, reload));
-    gulp.watch(config.assets + '/{img,fonts}/**', gulp.series(copy, reload));
+    gulp.watch(config.theme + "/**/*.{twig,php}", reload);
+    gulp.watch(config.assets + "/scss/**/*.scss", styles);
+    gulp.watch(config.assets + "/js/**/*.js", gulp.series(scripts, reload));
+    gulp.watch(config.assets + "/{img,fonts}/**", gulp.series(copy, reload));
 
     done();
-}
+};
 
-const reload = done => {
+const reload = (done) => {
     server.reload();
     done();
-}
+};
 
 //gulp release
-export const release = done => {
+export const release = (done) => {
     isProduction = true;
     config.output = config.dist;
     compile();
     done();
-}
+};
 
-const compile = gulp.series(clean, styles, scripts, copy, cleanScripts, rev, staticHeaders);
+const compile = gulp.series(
+    clean,
+    styles,
+    scripts,
+    copy,
+    cleanScripts,
+    rev,
+    staticHeaders
+);
 export const defaultTasks = gulp.series(clean, styles, copy, serve);
 
 export default defaultTasks;
