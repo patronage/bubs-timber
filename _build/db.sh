e43a474a13f4f9c51815ae6ae94b83a61513fc20
Cameron Corda <ccorda@gmail.com>
2022-09-21 15:18:15 -0700

    DB script error handling, docs

--- _build/db.sh
+++ _build/db.sh
@@ -3,13 +3,24 @@
 ## env export, with unset at end of script
 if [ -f ".env" ]; then
   export $(grep -v '^#' .env | xargs)
+else
+  echo ".env file required, please copy from the .env.sample"
+  exit 1;
 fi
 
+## Per Project Variables -- CUSTOMIZE THESE FIRST IN .ENV
 WORDPRESS_DB_USER="root"
 WORDPRESS_DB_PASSWORD="somewordpress"
 WORDPRESS_DB_NAME=${COMPOSE_PROJECT_NAME:-wordpress}
 DB_CONTAINER="${COMPOSE_PROJECT_NAME:-wordpress}_db_1"
 PRODUCTION_SSH="${COMPOSE_WPE_PRODUCTION}@${COMPOSE_WPE_PRODUCTION}.ssh.wpengine.net"
+
+# handle script errors, exit and kick you to working branch
+function error_exit {
+  echo "$1" 1>&2
+  echo "Aborting export attempt"
+  exit 1
+}
 
 function db_import() {
   sql=`ls -Art _data/* | tail -n 1`
@@ -42,16 +53,48 @@ function db_import() {
 }
 
 function db_export() {
-  echo "$PRODUCTION_SSH";
-  wp db export --add-drop-table --ssh=$PRODUCTION_SSH - | gzip > _data/$(date +'%Y-%m-%d-%H-%M-%S').sql.gz
-  echo "export complete";
+  local TARGET=${1}
+
+  if [ "$TARGET" = "staging" ]; then
+    SSH_TARGET=$STAGING_SSH
+  elif [ "$TARGET" = "production" ]; then
+    SSH_TARGET=$PRODUCTION_SSH
+  elif [ "$TARGET" = "development" ]; then
+    SSH_TARGET=$DEVELOPMENT_SSH
+  else
+    error_exit "Export task requires a target. Specify a target (staging, development, production)"
+  fi
+
+  echo "connecting to $SSH_TARGET"
+
+  status=$(ssh -o BatchMode=yes -o ConnectTimeout=5 $SSH_TARGET echo ok 2>&1)
+
+  if [[ $status == ok ]] ; then
+    echo "auth ok, proceeding with export"
+    if ! command -v wp &> /dev/null
+    then
+      error_exit "'wp' command could not be found. WP CLI local install required to export DB"
+    else
+      mkdir -p _data
+      filename=$(date +'%Y-%m-%d-%H-%M-%S').sql.zip
+      wp db export --add-drop-table --ssh=$SSH_TARGET - | gzip > _data/$(date +'%Y-%m-%d-%H-%M-%S').sql.gz
+      echo "export complete";
+    fi
+  elif [[ $status == "Permission denied"* ]] ; then
+    echo no_auth
+  elif [[ $status == "Host key verification failed"* ]] ; then
+    echo "host key not yet verified, please run: ssh $SSH_TARGET then try again"
+  else
+    echo "SSH couldn't connect, please check that environments are defined in your .env, and your SSH key is added to WP Engine"
+  fi
 }
 
 CALLED_FUNCTION=${1}
+TARGET=${2}
 
 if [ "$CALLED_FUNCTION" = "export" ]; then
-  echo "running DB export script"
-  db_export
+  echo "Running DB export for $TARGET"
+  db_export $TARGET
 elif [ "$CALLED_FUNCTION" = "import" ]; then
   echo "running DB import script"
   db_import
--- _build/deploy.sh
+++ _build/deploy.sh
@@ -1,10 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
 ## env export, with unset at end of script
 if [ -f ".env" ]; then
   export $(grep -v '^#' .env | xargs)
+else
+  echo ".env file required, please copy from the .env.sample"
+  exit 1;
 fi
 
+
+## Per Project Variables -- CUSTOMIZE THESE FIRST IN .ENV
 PRODUCTION_REMOTE="git@git.wpengine.com:production/${COMPOSE_WPE_PRODUCTION}.git"
 STAGING_REMOTE="git@git.wpengine.com:production/${COMPOSE_WPE_STAGING}.git"
 DEVELOPMENT_REMOTE="git@git.wpengine.com:production/${COMPOSE_WPE_DEVELOPMENT}.git"
@@ -13,131 +18,89 @@ GIT_NAME="Bubs Deploy"
 
 # read txt files with list of files to include/exclude
 function get_array {
-    i=0
-    while read line # Read a line
-    do
-        array[i]=$line # Put it into the array
-        i=$(($i + 1))
-    done < $1
+  i=0
+  while read line # Read a line
+  do
+    array[i]=$line # Put it into the array
+    i=$(($i + 1))
+  done < $1
 }
 
 # handle script errors, exit and kick you to working branch
 function error_exit {
-    echo "$1" 1>&2
-    echo "Aborting and returning to working branch."
-    git stash
-    git checkout $branch
-    exit 1
-}
-
-# Ref: https://stackoverflow.com/questions/8223906/how-to-check-if-remote-branch-exists-on-a-given-remote-repository
-# test if the branch is in the remote repository.
-# return 1 if its remote branch exists, or 0 if not.
-function is_in_remote() {
-    local remote=${1}
-    local branch=${2}
-    local existed_in_remote=$(git ls-remote --heads ${remote} ${branch})
-
-    if [[ -z ${existed_in_remote} ]]; then
-        return 1
-    else
-        return 0
-    fi
-}
-
-function wpe_deploy() {
-    local TARGET=${1}
-    local REMOTE=${2}
-    local BRANCH=${3}
-
-    echo "Pushing to ${TARGET}..."
-    git remote rm ${TARGET}
-    git remote add ${TARGET} ${REMOTE}
-    cd ..
-
-    # check if master exists on remote
-    if is_in_remote ${TARGET} "master"; then
-        echo "WP engine ready for deploy, proceeding"
-        git push -f ${TARGET} deploy:master
-        echo "Returning to working branch."
-        git stash
-        git checkout ${BRANCH}
-    else
-        echo "First time deploy, prepping remote with empty file"
-        mkdir tmp
-        cd tmp
-        echo 'Hello, world.' > tmp.txt
-        git init
-        git add . && git commit --no-verify -am "comment"
-        git remote add ${TARGET} ${REMOTE}
-        git push -f ${TARGET} master
-        echo "Remote ready, cleaning up"
-        cd ..
-        rm -rf tmp
-        echo "Remote now ready, please try deploying again to populate."
-        echo "Returning to working branch."
-        git stash
-        git checkout ${BRANCH}
-    fi
+  echo "$1" 1>&2
+  echo "Aborting and returning to working branch."
+  git stash
+  git checkout $branch
+  exit 1
 }
 
 # check environment to ensure we should proceed with build
 if [[ -n $(git status --porcelain) ]]; then
-    error_exit "There are uncommited changes -- please commit before proceeding."
+  error_exit "There are uncommited changes -- please commit before proceeding."
 else
 
-    if [ `git branch --list deploy` ]; then
-        echo "Branch deploy already exists, deleting then continuing"
-        git branch -D deploy
-    fi
+  if [ `git branch --list deploy` ]; then
+    echo "Branch deploy already exists, deleting then continuing"
+    git branch -D deploy
+  fi
 
-    # save current branch to a variable
-    branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+  # save current branch to a variable
+  branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 
-    git checkout -b deploy
-    npx gulp release || error_exit "Gulp release failed."
+  git checkout -b deploy
+  npx gulp release || error_exit "Gulp release failed."
 
-    echo "Adding built files that are normally .gitignored..."
-    array=()
-    get_array "_build/.deploy_include.txt"
-    for e in "${array[@]}"
-    do
-        printf "$e\n"
-        git add "$e" -f
-    done
+  echo "Adding built files that are normally .gitignored..."
+  array=()
+  get_array "_build/.deploy_include.txt"
+  for e in "${array[@]}"
+  do
+    printf "$e\n"
+    git add "$e" -f
+  done
 
-    echo "Removing files we don't want on the server"
-    array=()
-    get_array "_build/.deploy_exclude.txt"
-    for e in "${array[@]}"
-    do
-        git rm -r --ignore-unmatch "$e"
-    done
+  echo "Removing files we don't want on the server"
+  array=()
+  get_array "_build/.deploy_exclude.txt"
+  for e in "${array[@]}"
+  do
+    git rm -r --ignore-unmatch "$e"
+  done
 
-    echo "Committing build changes"
-    git commit -m "Committing build changes"
+  echo "Committing build changes"
+  git commit -m "Committing build changes"
 
-    if [ "$1" = "staging" ]; then
-        wpe_deploy "staging" ${STAGING_REMOTE} $branch
+  if [ "$1" = "staging" ]; then
+    echo "Pushing to staging..."
+    git remote rm staging
+    git remote add staging ${STAGING_REMOTE}
+    git push -f staging deploy:master
+    echo "Returning to working branch."
+    git stash
+    git checkout $branch
 
-    elif [ "$1" = "development" ] || [ "$1" = "dev" ]; then
-        wpe_deploy "development" ${DEVELOPMENT_REMOTE} $branch
+  elif [ "$1" = "production" ]; then
+    echo "Pushing to production..."
+    git remote rm production
+    git remote add production ${PRODUCTION_REMOTE}
+    git push -f production deploy:master
+    echo "Returning to working branch."
+    git stash
+    git checkout $branch
 
-    elif [ "$1" = "production" ] || [ "$1" = "prod" ]; then
-        wpe_deploy "production" ${PRODUCTION_REMOTE} $branch
+  else
+    error_exit "No deploy conditions met."
+  fi
 
-    else
-        error_exit "No deploy conditions met. Specify a target (staging, development, production)"
-    fi
-
-    ## test for git response, and if error code, bail out of travis with an error code
-    success=$?
-    if [[ $success -eq 0 ]];
-    then
-        echo "Build script complete"
-    else
-        error_exit "Something went wrong with the deploy."
-    fi
+  ## test for git response, and if error code, bail out of travis with an error code
+  success=$?
+  if [[ $success -eq 0 ]];
+  then
+    echo "Build script complete"
+  else
+    error_exit "Something went wrong with the deploy."
+  fi
 fi
 
 if [ -f ".env" ]; then
