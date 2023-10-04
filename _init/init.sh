e5de9b7b820e88d6897a6ba645717e2130505b74
Cameron Corda <ccorda@gmail.com>
2023-10-03 19:52:11 -0700

    remove duplicate cp in init

--- _init/init.sh
+++ _init/init.sh
@@ -19,7 +19,6 @@ cp -n _init/local-plugins.php wp-content/mu-plugins/local-plugins.php 2>/dev/nul
 cp -n _init/docker-compose.local.yml docker-compose.local.yml 2>/dev/null || :
 cp -n _init/gulpconfig.json gulpconfig.json 2>/dev/null || :
 cp -n _init/local.sql _data/local.sql 2>/dev/null || :
-cp -n _init/local-plugins.php wp-content/mu-plugins/local-plugins.php 2>/dev/null || :
 mv _init/README.md README.md 2>/dev/null || :
 
 # WP permissions
